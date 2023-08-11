c08ee59f1a126d972d69c75b8aebf11f2862460e
root <root@epd8oaqr4o5snsfh52j5.auto.internal>
2023-08-11 08:53:12 +0000

    Corrected after review

--- backend/kittygram_backend/settings.py
+++ backend/kittygram_backend/settings.py
@@ -8,9 +8,7 @@ load_dotenv()
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-SECRET_KEY = os.getenv(
-    'SECRET_KEY',
-)
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 DEBUG = bool(strtobool(os.getenv('DEBUG', 'False')))
 
